068ab07af538cbe2caadd9f4cd4344647a29dc30
Stefania Damato <stefania.damato.15@um.edu.mt>
2018-04-25 23:51:52 +0200

    Parsed variable declaration

---
+++ AST/ASTNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stefaniatadama on 22/04/2018.
+//
+
+#include "ASTNode.h"
---
+++ AST/ASTNode.h
@@ -0,0 +1,14 @@
+//
+// Created by stefaniatadama on 22/04/2018.
+//
+
+#ifndef ASSIGNMENT_ASTNODE_H
+#define ASSIGNMENT_ASTNODE_H
+
+
+class ASTNode {
+
+};
+
+
+#endif //ASSIGNMENT_ASTNODE_H
